f96050b2a6f800c2d2614c0eee76bded978f8203
MuhammadZubair210 <mzubairabdullah210@gmail.com>
2022-07-02 18:18:07 +0500

    Twitter module configured for a new api

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ import {
 } from "@nfteyez/sol-rayz";
 import axios from "axios";
 import express from "express";
+import express from "twitter-lite";
 
 var app = express();
 const port = process.env.PORT || 3000;
+const user = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+});
 
 const getAllNftData = async (token) => {
   try {
-    
-
     const publicAddress = await resolveToWalletAddress({
       text: token,
     });
@@ -19,8 +22,6 @@ const getAllNftData = async (token) => {
     const nftArray = await getParsedNftAccountsByOwner({
       publicAddress,
     });
-
-    // console.log(nftArray)
 
     return nftArray;
   } catch (error) {
@@ -31,8 +32,7 @@ const getAllNftData = async (token) => {
 const getNftTokenData = async (token) => {
   try {
     let nftData = await getAllNftData(token);
-    var data = nftData
-    //  Object.keys(nftData).map((key) => nftData[key]);
+    var data = nftData;
     let arr = [];
     let n = data.length;
     let obj = {};
@@ -42,15 +42,12 @@ const getNftTokenData = async (token) => {
         obj[data[i].data["symbol"]] && obj[data[i].data["symbol"]].length > 0
           ? [...obj[data[i].data["symbol"]], val.data]
           : [val.data];
-          // console.log(val)
     }
-    console.log(obj);
     return obj;
   } catch (error) {
     console.log(error);
   }
 };
-// getNftTokenData("7oAw3KSBeuuXb4p6KZL2BmnHbnXUzoF8dxAkXTMbMaiP");
 
 app.get("/:token", async (req, res) => {
   let response = await getNftTokenData(req.params.token);
